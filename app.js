9094931c7139df62a2ca0a3d4da256a412b8ea2e
chris <chris.turp1@gmail.com>
2021-10-13 23:59:41 +0200

    Add item with submit

--- app.js
+++ app.js
@@ -2,43 +2,35 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+var items = ["Buy Food", "Cook Food", "Eat Foor"];
 
-app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ // this to be written after the app=express():
+  extended: true
+}));
+
+app.set("view engine", "ejs"); // to set up ejs
 
 app.get("/", function(req, res) {
   var today = new Date();
-  var currentDay = today.getDay();
-  var day = "";
+  var options = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long'
+  };
 
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    default:
-  }
+  var day = today.toLocaleDateString("en-US", options);
 
   res.render("list", {
-    kindOfDay: day
+    kindOfDay: day,
+    newListItems: items
   });
 });
+
+app.post("/", function(req, res) {
+  var item = req.body.newItem;
+  items.push(item);
+  res.redirect("/");
+})
 
 
 
