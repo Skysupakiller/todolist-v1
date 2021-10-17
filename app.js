2bc3ae4814d230c7f70d15350b76c75f2b8ac393
chris <chris.turp1@gmail.com>
2021-10-17 17:49:36 +0200

    Adding footer header

--- app.js
+++ app.js
@@ -1,43 +1,54 @@
+//jshint esversion:6
+
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
-var items = ["Buy Food", "Cook Food", "Eat Foor"];
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = [];
 
 app.use(bodyParser.urlencoded({ // this to be written after the app=express():
   extended: true
 }));
+app.use(express.static("public")); //to add image or css
 
 app.set("view engine", "ejs"); // to set up ejs
 
 app.get("/", function(req, res) {
-  var today = new Date();
-  var options = {
-    weekday: 'long',
-    day: 'numeric',
-    month: 'long'
-  };
 
-  var day = today.toLocaleDateString("en-US", options);
-
+  //module return valure here
+  const day = date.getDate();
   res.render("list", {
-    kindOfDay: day,
+    listTitle: day,
     newListItems: items
   });
 });
 
+
 app.post("/", function(req, res) {
-  var item = req.body.newItem;
-  items.push(item);
-  res.redirect("/");
+  const item = req.body.newItem;
+
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+});
+
+
+app.get("/work", function(req, res) {
+  res.render("list", {
+    listTitle: "Work",
+    newListItems: workItems
+  });
+});
+
+app.get("/about", function(req, res) {
+  res.render("about");
 })
-
-
-
-
-
-
-
 
 
 app.listen(3000, function() {
